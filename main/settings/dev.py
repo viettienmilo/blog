17ed677e5b659888a1b2205a2b9d0ae81a54cb90
Nguyen VIet Tien <viettien.milo@gmail.com>
2021-07-26 16:11:40 +0700

    Start Project Blog

---
+++ main/settings/dev.py
@@ -0,0 +1,9 @@
+from ._base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = 'django-insecure-b^n1)7qc2t5jp226b1!oxb2a2%d=h35jb89e%)hg9jnt%e^ter'
